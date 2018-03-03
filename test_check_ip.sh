6e2c68e620bb2d33834587f2dd4bfa2b5353c2a5
Vladimir <originalblack@mail.ru>
2018-03-04 01:19:53 +0300

    fix regexp in dns_ip variable

--- test_check_ip.sh
+++ test_check_ip.sh
@@ -3,7 +3,7 @@ JUN_NUM=$( lldpctl |grep SysName|head -n 1|awk {'print $2'}|sed 's/jun//' )
 GOOD_OCTET=$(($JUN_NUM * 2 % 256))
 IP_ON_HOST=$( ip addr|grep 3001|grep inet|awk {'print $2'}|sed 's/\/23//' )
 IP_OCTET=$( echo $IP_ON_HOST|cut -d . -f 3 )
-DNS_IP=$( host `hostname|sed -E 's/(i|q)/e/'`|awk {'print $4'} )
+DNS_IP=$( host `hostname|sed -E 's/(\.(i|q))$/\.e/'`|awk {'print $4'} )
 
 if [[ "$DNS_IP" != "$IP_ON_HOST" ]]; then
         echo "DNS PTR record not equal ip on host "
